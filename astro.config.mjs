5112b08babcbd273ac65fd354b910b47a583424b
jair david <cjair759@gmail.com>
2025-09-06 05:09:09 -0500

    railway11

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
 });
