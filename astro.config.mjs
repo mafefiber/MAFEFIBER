5d519df8e830bfa98b3927bd8a03d0ea458ae8b4
jair david <cjair759@gmail.com>
2025-09-06 03:12:40 -0500

    version semi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import staticAdapter from '@astrojs/adapter-static';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  adapter: staticAdapter(),
+});
